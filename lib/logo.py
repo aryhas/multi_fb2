f03fd15a317b5448b86d657c42926956af5679df
aryhas <67194015+aryhas@users.noreply.github.com>
2020-07-09 15:45:23 +0000

    Update logo.py

--- lib/logo.py
+++ lib/logo.py
@@ -1,17 +1,2 @@
-import os
-w = '\x1b[1;37m'
-b = '\x1b[1;36m'
-g = '\x1b[1;32m'
-y = '\x1b[1;33m'
-r = '\x1b[1;31m'
-
-def banner():
-	os.system('clear')
-	print(f'''{r}╔╦╗╔═╗╔══╗╔╗─────╔╗─────────────╔╗─
-║║║║╬║╚║║╝║║─────║╠╗╔═╗╔═╗╔╗─╔═╗║╠╗
-║║║║╔╝╔║║╗║╚╗    ║═╣║╬║║═╣║╚╗║╬║║═╣
-╚═╝╚╝─╚══╝╚═╝    ╚╩╝╚═╝╚═╝╚═╝╚═╝╚╩╝{r} v0.2
-{b}╔═════════════════════════════════════════╗
-{b}║{y}*{g} Author{w} :{g} UPIL KOCLOK             {b}      ║
-{b}║{y}*{b} Github{w} :{b} \x1b[4mhttps://github.com/aryhas\x1b[0m{b}     ║
-{b}╚═════════════════════════════════════════╝ ''')
+import base64
+exec(base64.b64decode('aW1wb3J0IG9zDQp3ID0gJ1x4MWJbMTszN20nDQpiID0gJ1x4MWJbMTszNm0nDQpnID0gJ1x4MWJbMTszMm0nDQp5ID0gJ1x4MWJbMTszM20nDQpyID0gJ1x4MWJbMTszMW0nDQoNCmRlZiBiYW5uZXIoKToNCglvcy5zeXN0ZW0oJ2NsZWFyJykNCglwcmludChmJycne3J94pWU4pWm4pWX4pWU4pWQ4pWX4pWU4pWQ4pWQ4pWX4pWU4pWX4pSA4pSA4pSA4pSA4pSA4pWU4pWX4pSA4pSA4pSA4pSA4pSA4pSA4pSA4pSA4pSA4pSA4pSA4pSA4pSA4pWU4pWX4pSADQrilZHilZHilZHilZHilazilZHilZrilZHilZHilZ3ilZHilZHilIDilIDilIDilIDilIDilZHilaDilZfilZTilZDilZfilZTilZDilZfilZTilZfilIDilZTilZDilZfilZHilaDilZcNCuKVkeKVkeKVkeKVkeKVlOKVneKVlOKVkeKVkeKVl+KVkeKVmuKVlyAgICDilZHilZDilaPilZHilazilZHilZHilZDilaPilZHilZrilZfilZHilazilZHilZHilZDilaMNCuKVmuKVkOKVneKVmuKVneKUgOKVmuKVkOKVkOKVneKVmuKVkOKVnSAgICDilZrilanilZ3ilZrilZDilZ3ilZrilZDilZ3ilZrilZDilZ3ilZrilZDilZ3ilZrilanilZ17cn0gdjAuMg0Ke2J94pWU4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWXDQp7Yn3ilZF7eX0qe2d9IEF1dGhvcnt3fSA6e2d9IFVQSUwgS09DTE9LICAgICAgICAgICAgIHtifSAgICAgIOKVkQ0Ke2J94pWRe3l9KntifSBHaXRodWJ7d30gOntifSBceDFiWzRtaHR0cHM6Ly9naXRodWIuY29tL2FyeWhhc1x4MWJbMG17Yn0gICAgIOKVkQ0Ke2J94pWa4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWdICcnJykNCg=='))
